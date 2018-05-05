2c31ba217d6c23fc6ae0b8dbf01c213812e5ddce
Noah Strayve <noahstrayve@gmail.com>
2018-05-05 10:32:48 -0500

    pushing portfolio

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name, eaten) VALUES ('Big Kahuna Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Royale with Cheese', true);
+INSERT INTO burgers (name, eaten) VALUES ('Goodburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Baby You Can Chive My Car Burger  (comes w/ chives)', true);
+INSERT INTO burgers (name, eaten) VALUES ('Krusty Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('White Castle Slider', true);
+INSERT INTO burgers (name, eaten) VALUES ('Jucy Lucy', false);
